8bcd5fca2cef8088db8769fd28222957e308f530
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-06-30 21:24:01 -0700

    Persistencia
    Creación del proyecto Persistencia-Juatsapp
    
    - Incluye DAOs para Chat, Mensaje y -Usuario
    
    - Clase que permite conectarse a Mongo
    
    - El nombre de la base de datos será "juatsapp"

---
+++ juatsappPersistencia/src/main/java/com/mycompany/juatsapppersistencia/JuatsappPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juatsapppersistencia;
+
+/**
+ *
+ * @author eduar
+ */
+public class JuatsappPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ juatsappPersistencia/src/main/java/conexion/ConexionBD.java
@@ -0,0 +1,44 @@
+package conexion;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+
+
+/**
+ *
+ * @author eduar
+ */
+public class ConexionBD {
+    private static final String CONNECTION_STRING = "mongodb://localhost:27017";
+    private static final String DATABASE_NAME = "juatsapp";
+    private static MongoClient mongoClient = null;
+    private static MongoDatabase database = null;
+
+    public static MongoDatabase getDatabase() {
+        if (database == null) {
+            try {
+                MongoClient mongoClient = (MongoClient) MongoClients.create(CONNECTION_STRING);
+                database = mongoClient.getDatabase(DATABASE_NAME);
+            } catch (Exception e) {
+                System.err.println("Error connecting to MongoDB: " + e.getMessage());
+            }
+        }
+        return database;
+    }
+
+    public static void verificarConexionBD() {
+        try {
+            System.out.println("Verificando conexión a la base de datos...");
+            MongoDatabase db = getDatabase();
+            System.out.println("Colecciones disponibles en la base de datos '" + DATABASE_NAME + "':");
+            for (String name : db.listCollectionNames()) {
+                System.out.println(name);
+            }
+            System.out.println("Conexión exitosa a la base de datos.");
+        } catch (Exception e) {
+            System.err.println("Error al verificar la conexión con la base de datos: " + e.getMessage());
+        }
+    }
+}
